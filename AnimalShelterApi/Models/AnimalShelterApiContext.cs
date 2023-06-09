adea8c92e965a446597b28e6bb563ad0d73fb16e
Laura Hope <ssteamlaura@gmail.com>
2023-06-09 08:37:39 -0700

    Add AnimalShelterApiContext file and data to appsettings.json

---
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelterApi.Models
+{
+  public class AnimalShelterApiContext : DbContext
+  {
+    public DbSet<Animal> Animals { get; set; }
+
+    public AnimalShelterApiContext(DbContextOptions<AnimalShelterApiContext> options) : base(options)
+    {
+    }
+  }
+}
