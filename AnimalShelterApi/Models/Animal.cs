93cda9215fa032778508c7d2b7de80cf729ddad3
Laura Hope <ssteamlaura@gmail.com>
2023-06-09 15:26:19 -0700

    Add client.solution

--- AnimalShelterApi/Models/Animal.cs
+++ AnimalShelterApi/Models/Animal.cs
@@ -5,7 +5,6 @@ namespace AnimalShelterApi.Models
     public int AnimalId { get; set; }
     public string Name { get; set; }
     public string Species { get; set; }
-
     public string Breed { get; set;}
     public int Age { get; set; }
   }
